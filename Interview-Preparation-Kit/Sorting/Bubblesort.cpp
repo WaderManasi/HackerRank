3fd1634f9db1fd5d2f4beb9ef7b28a6ea66ffb00
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-08-10 16:52:12 +0530

    added

---
+++ Interview-Preparation-Kit/Sorting/Bubblesort.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> split_string(string);
+
+//Solution:: the countSwaps function below.
+void countSwaps(vector<int> a) {
+    int ct=0;
+    int n=a.size();
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                swap(a[j],a[j+1]);
+                ct++;
+            }
+        }
+    }
+    cout<<"Array is sorted in "<<ct<<" swaps.\n";
+    cout<<"First Element: "<<a[0]<<"\n";
+    cout<<"Last Element: "<<a[n-1]<<"\n";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split_string(a_temp_temp);
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    countSwaps(a);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
