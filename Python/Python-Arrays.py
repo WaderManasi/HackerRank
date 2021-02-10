dbdc07ec2a599c6c3c8f9b519d612f09fc9976c7
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-10 14:45:10 +0530

    Python

---
+++ Python/Python-Arrays.py
@@ -0,0 +1,10 @@
+import numpy
+
+def arrays(arr):
+    a = numpy.array(arr)
+    b = a[::-1]
+    return numpy.array(b,float)
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
