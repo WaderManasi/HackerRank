38b23fac1fb5ef94288bc46b301a27269257f540
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-12 11:59:47 +0530

    C++

---
+++ C++/C++-MultiLevelInheritance.cpp
@@ -0,0 +1,27 @@
+//User function Template for C++
+
+//MultiLevel Inheritance in CPP
+class test: public student 
+{
+    public:
+        float marks[5];
+        void set_marks(float arr[])
+        {
+            for(int i=0;i<5;i++)
+            marks[i]=arr[i];
+        }
+};
+
+class result: public test
+{
+    public:
+    void display()
+    {
+        float total=0,average=0;
+        for(int i=0;i<5;i++)
+        {
+             total += marks[i];
+        }
+        cout<<roll_number<<" "<<total<<" "<<(total)/5<<"\n";
+    }
+};
