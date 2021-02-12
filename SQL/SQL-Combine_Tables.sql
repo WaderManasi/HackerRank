c7d8ec557bec29638ef676caec660ebfa6f57f9c
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-12 22:27:26 +0530

    Combining two tables by using joins

--- SQL/SQL-Combine_Tables.sql
+++ SQL/SQL-Combine_Tables.sql
@@ -1,4 +1,5 @@
 --Leetcode solution
 
 /* Write your PL/SQL query statement below */
+--Left join
 select  FirstName,LastName,City,State from Person left join Address on Person.PersonId = Address.PersonId;
