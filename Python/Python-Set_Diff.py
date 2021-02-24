200fe3c8ac99faca6edf93f19b40ca8e587312bc
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-24 19:45:47 +0530

    Set Difference

---
+++ Python/Python-Set_Diff.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+a1=set(map(int,input().split()))
+b=int(input())
+b1=set(map(int,input().split()))
+print(len(a1.difference(b1)))
