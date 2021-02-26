77b4f652e33a68a15a1d62be4e6549c88e938285
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-26 23:36:53 +0530

    JS

---
+++ Javascript/Javascript-Day_7Regex2.js
@@ -0,0 +1,14 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    
+    let re=new RegExp("^(Mr|Mrs|Ms|Dr|Er)(\\.)([a-zA-Z])+$");
+ 
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
