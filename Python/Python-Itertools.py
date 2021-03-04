b2c4668c330b13ba77d8d2347e789388428001d1
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-03-04 09:06:43 +0530

    Cartesian product

---
+++ Python/Python-Itertools.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import product
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+print(*product(A,B))
